c4c6647cd050ca5f78f5d89120ae7b02a8a938df
pankaj2312002 <pankaj707165@gmail.com>
2025-02-03 10:15:32 +0530

    cors url added

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['https://study-hub-red.vercel.app/','http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
